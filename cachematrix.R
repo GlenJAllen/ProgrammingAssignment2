f07e20800c570592d998dcfec85d2b1d90c3a360
Glen <glen.j.o.allen@gmail.com>
2017-02-19 18:05:40 -0500

    Improved Comments

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,8 @@
 ## Input: an invertible matrix
 ## Ouput: a list of functions to:
 ##  1. Change the value stored from input
-##  2. Invert the matrix 
-##  3. Change the value of the stored inverted matrix
+##  2. Return the value stored from input 
+##  3. Set the value of the stored inverted matrix
 ##  4. Return the inverted matrix
 makeCacheMatrix <- function(x = matrix()) {
   
@@ -29,7 +29,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Returns a matrix that is the inverse of 'x' 
+## Returns a matrix that is the inverse of a matrix stored in 'x' 
 ## where 'x' is the the result of a call to makeCacheMatrix
 cacheSolve <- function(x, ...) {
   
